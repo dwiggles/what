a308e35677e1135e0569637b287377366527e6c8
Dan Wigglesworth <dwiggles@kewbeach.ca>
2020-06-14 22:42:33 -0400

    first time with jupytext

---
+++ jupytext_trial.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.5.0
+#   kernelspec:
+#     display_name: Python 3
+#     name: python3
+# ---
+
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+
+
